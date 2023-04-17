5ea048fed63086f763ba7e1fd2dee2e21065c95b
koffi-wallez <127743441+koffi-wallez@users.noreply.github.com>
2023-04-17 03:19:26 +0200

    Add files via upload

---
+++ Runtrack python/jour2/job2/main.py
@@ -0,0 +1,9 @@
+
+def My_print_name(name):
+    print("Votre Nom est :", name, "!")
+
+My_print_name(name="messi")
+My_print_name(name="ronaldo")
+My_print_name(name="benzema")
+My_print_name(name="xavi")
+My_print_name(name="neymar")
